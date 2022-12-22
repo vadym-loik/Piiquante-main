8382b90fcc2e9f2676033edf6f0f7e11d10bea48
vadym-loik <loikfit@gmail.com>
2022-12-22 16:36:49 +0100

    modified:   backend/controllers/user.js deleted:    backend/middleware/password.js deleted:    backend/models/password.js modified:   backend/models/user.js modified:   backend/routes/user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,27 +1,22 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 
 // user signup controller
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    user
-      .save()
-      .then(() => {
-        res.status(201).json({
-          message: 'User added successfully!',
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          error: error,
-        });
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
       });
-  });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: 'User created!' }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 // user login controller
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+// user schema username and PW (unique email)
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
