9f2d8cbc9862b667251c82e497ee554de397aaee
vadym-loik <loikfit@gmail.com>
2022-12-26 15:08:04 +0100

    modified:   backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 // const auth = require('./middleware/auth');
 
 const userRoutes = require('./routes/user');
@@ -35,5 +36,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/api/auth', userRoutes);
+app.use('./images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
