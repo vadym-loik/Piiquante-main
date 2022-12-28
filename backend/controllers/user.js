2a39f5a966654cab0503885cd22506b2b93bb5b5
vadym-loik <loikfit@gmail.com>
2022-12-28 17:56:38 +0100

    update user.js file in controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,9 +12,12 @@ exports.signup = (req, res, next) => {
     user
       .save()
       .then(() => {
-        res.status(201).json({
-          message: 'User added successfully!',
-        });
+        res
+          .status(201)
+          .json({
+            message: 'User added successfully!',
+          })
+          .catch((error) => res.status(400).json({ error }));
       })
       .catch((error) => {
         res.status(500).json({
