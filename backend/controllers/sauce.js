8771d1e4f6b0c246c244735fc178a257f1fb14b0
vadym-loik <loikfit@gmail.com>
2023-01-02 20:24:52 +0100

    edit controllers/sauce.js and controllers/user.js

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -48,7 +48,7 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlike(`images/${filename}`, () => {
+      fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Object deleted.' }))
           .catch((error) => res.status(400).json({ error }));
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,12 +12,9 @@ exports.signup = (req, res, next) => {
     user
       .save()
       .then(() => {
-        res
-          .status(201)
-          .json({
-            message: 'User added successfully!',
-          })
-          .catch((error) => res.status(400).json({ error }));
+        res.status(201).json({
+          message: 'User added successfully!',
+        });
       })
       .catch((error) => {
         res.status(500).json({
